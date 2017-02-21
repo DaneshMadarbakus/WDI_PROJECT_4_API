7630c207784562f0e7dd50f18dc4f1d7a00566de
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-02-21 16:01:58 +0000

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-[User, Review, Request, Suit].each(&:destroy_all)
+[Review, Request, Suit, User].each(&:destroy_all)
 
 u1 = User.create!(email: 'danesh@danesh.com', profile_img: 'http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/13713975_1570935016543603_921887456_n.jpg', password: 'password', password_confirmation: 'password', username: 'theheartbreakkid', telephone_no: '08765456432', first_name: 'Danesh', last_name: 'Madarbakus', address_first_line: 'General Assembly', address_second_line: 'Whitechape', address_city: 'London', address_postcode: 'E1 7PT')
 
@@ -16,11 +16,11 @@ u1.suits.create(image: 'http://cdn.yoox.biz/49/49221711OV_11_n_f.jpg', title: 'S
 
 u1.suits.create(image: 'https://www.reiss.com/media/product/images/019/046/31-1-250x287x85.jpg', title: 'Smart', brand: 'Reiss', colour: 'blue', jacket_chest: '42', jacket_length: '42', jacket_shoulder_length: '32', jacket_sleeve: '32', jacket_waist: '38', trouser_waist: '38', trouser_length: '38', trouser_front_rise: '38', notes: 'Good for casual and formal occasions', deposit: 450, daily_price: 30, fit: 'Slim')
 
-u2.suits.create(image: 'https://i1.adis.ws/i/tom_ford/31YL4C-922R70_GRY_OS_A?$pdp_hero_mob$.jpg', title: 'Tom Ford', brand: 'suity', colour: 'brown', jacket_chest: '46', jacket_length: '46', jacket_shoulder_length: '46', jacket_sleeve: '48', jacket_waist: '42', trouser_waist: '42', trouser_length: '52', trouser_front_rise: '42', notes: 'I don't pop molly I rock Tom Ford', deposit: 500, daily_price: 40, fit: 'Skinny')
+u2.suits.create(image: 'https://i1.adis.ws/i/tom_ford/31YL4C-922R70_GRY_OS_A?$pdp_hero_mob$.jpg', title: 'Tom Ford', brand: 'suity', colour: 'brown', jacket_chest: '46', jacket_length: '46', jacket_shoulder_length: '46', jacket_sleeve: '48', jacket_waist: '42', trouser_waist: '42', trouser_length: '52', trouser_front_rise: '42', notes: 'I dont pop molly I rock Tom Ford', deposit: 500, daily_price: 40, fit: 'Skinny')
 
 u5.suits.create(image: 'https://media.gucci.com/style/DarkGray_South_0_160_540x540/1459879208/406135_Z491B_4251_001_100_0000_Light-Monaco-cotton-wool-jacquard-suit.jpg', title: 'Gucci Kingpin Suit', brand: 'Gucci', colour: 'blue', jacket_chest: '42', jacket_length: '42', jacket_shoulder_length: '38', jacket_sleeve: '38', jacket_waist: '38', trouser_waist: '38', trouser_length: '38', trouser_front_rise: '38', notes: 'Once worn by McGregor himself', deposit: 700, daily_price: 45, fit: 'Skinny')
 
-u5.suits.create(image: 'http://demandware.edgesuite.net/sits_pod37/dw/image/v2/ABAO_PRD/on/demandware.static/-/Sites-ver-master-catalog/default/dw5db663be/original/90_A72391-A220833_A616_10_SingleBreastedWoolBlendSuit-Suits-versace-online-store_0_0.png', title: 'All About Versace', brand: 'Versace', colour: 'Blue', jacket_chest: '42', jacket_length: '42', jacket_shoulder_length: '38', jacket_sleeve: '38', jacket_waist: '38', trouser_waist: '38', trouser_length: '38', trouser_front_rise: '38', notes: 'Now it's all about Versace you catting my style!', deposit: 580, daily_price: 40, fit: 'Skinny')
+u5.suits.create(image: 'http://demandware.edgesuite.net/sits_pod37/dw/image/v2/ABAO_PRD/on/demandware.static/-/Sites-ver-master-catalog/default/dw5db663be/original/90_A72391-A220833_A616_10_SingleBreastedWoolBlendSuit-Suits-versace-online-store_0_0.png', title: 'All About Versace', brand: 'Versace', colour: 'Blue', jacket_chest: '42', jacket_length: '42', jacket_shoulder_length: '38', jacket_sleeve: '38', jacket_waist: '38', trouser_waist: '38', trouser_length: '38', trouser_front_rise: '38', notes: 'Now its all about Versace you catting my style!', deposit: 580, daily_price: 40, fit: 'Skinny')
 
 u2.requests.create!(start_time: Time.now + (60*60*24), end_time: Time.now + (60*60*48), suit_id: 1)
 
