36f3b8b805574d2e5374032bade4e53d853741cd
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-02-21 11:51:55 +0000

    added index route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:show]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
 
   # GET /users/1
   def show
